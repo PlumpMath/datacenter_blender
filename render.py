bae3e40e1a257bb9ce53389c9a7cef86e3097081
Ludvig Rhodin <rhodin.ludvig@gmail.com>
2014-12-12 21:58:43 +0000

    fixed rsync issues

--- render.py
+++ render.py
@@ -15,9 +15,9 @@ def callback(ch, method, properties, body):
     bodylist = body.split()
     project = bodylist[0]
     frame = bodylist[1]
-    os.system("/usr/bin/rsync -r webserver.local:~/projects/%s ~/%s" % (project, project))
+    os.system("/usr/bin/rsync -r webserver.local:~/projects/%s ~/projects" % (project))
     os.system("/usr/bin/blender -b /home/rhodin/projects/%s/%s.blend -o /home/rhodin/renders/%s/ -F JPEG -x %s -f %s" % (project,project,project,frame,frame))
-    os.system("/usr/bin/scp /home/rhodin/renders/%s/%s webserver.local:/home/rhodin/renders/%s/" % (project, frame.zfill(4), project))
+    os.system("/usr/bin/scp /home/rhodin/renders/%s/%s.jpg webserver.local:/home/rhodin/renders/%s/" % (project, frame.zfill(4), project))
 
 channel.basic_consume(callback,
                       queue='render',
