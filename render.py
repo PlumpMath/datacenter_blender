60e56939f2bc9f7c7bff89c929d01db560e16d9a
Ludvig Rhodin <rhodin.ludvig@gmail.com>
2014-12-12 17:01:28 +0000

    Added render file

---
+++ render.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import pika
+import os
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='render')
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+    bodylist = body.split()
+    project = bodylist[0]
+    frame = bodylist[1]
+    os.system("/usr/bin/blender -b /home/rhodin/projects/%s/%s.blend -o /home/rhodin/projects/%s/render/ -F JPEG -x %s -f %s" % (project,project,project,frame,frame))
+
+channel.basic_consume(callback,
+                      queue='render',
+                      no_ack=True)
+
+channel.start_consuming()
