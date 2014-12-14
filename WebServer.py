33908da2b591873bb9b6fc6d288202247eb86602
Ludvig Rhodin <rhodin.ludvig@gmail.com>
2014-12-14 14:36:50 +0000

    Added project uploading

--- WebServer.py
+++ WebServer.py
@@ -1,6 +1,10 @@
 import os
 from flask import Flask, request, redirect, url_for, send_file
+from werkzeug import secure_filename
 import pika
+
+UPLOAD_FOLDER = '/home/rhodin/projects/'
+ALLOWED_EXTENSIONS = set(['zip'])
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -9,6 +13,31 @@ channel = connection.channel()
 channel.queue_declare(queue='render')
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+# To upload a project
+# Project should be in format:
+# Zip file should be named <project_name>.zip
+# Root directory should be named <project_name>
+# .blend file in root directory
+@app.route('/upload', methods=['POST'])
+def upload_file():
+      file = request.files['file']
+      if file and allowed_file(file.filename):
+          filename = secure_filename(file.filename)
+          file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+          rawprojectname = filename.rsplit('.', 1)[0]
+          os.system('rm -rf /home/rhodin/projects/%s' % (rawprojectname))
+          os.system('unzip -d /home/rhodin/projects /home/rhodin/projects/%s' % (filename))
+          os.system('rm /home/rhodin/projects/%s' % (filename))
+          os.system('mkdir /home/rhodin/renders/%s' % (rawprojectname))
+          return '%s added to database.\n' % (filename)
+      return 'Please upload a .zip file containing a Blender project.\n'
 
 # To request a render
 @app.route("/render/<project>/<start>/<end>")
